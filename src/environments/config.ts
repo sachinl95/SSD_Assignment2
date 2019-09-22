c14a445d64208223b56299ae102f16e7e8400a53
Sachin Pathirana <sachin@datacaptureexperts.com.au>
2019-09-22 17:21:24 +0530

    config

---
+++ src/environments/config.ts
@@ -0,0 +1,9 @@
+export const configPlaceholder = {
+  apiKey: 'AIzaSyD2Bew8MJrM6CNd8508XcFmCHM8HcXR35Y',
+  authDomain: 'ssdassignment-41248.firebaseapp.com',
+  databaseURL: 'https://ssdassignment-41248.firebaseio.com',
+  projectId: 'ssdassignment-41248',
+  storageBucket: 'ssdassignment-41248.appspot.com',
+  messagingSenderId: '796793947598',
+  appId: '1:796793947598:web:7aa4b940706001eff3979f'
+};
